6068b153fe7d1be6a85f35041c772226ea9c9fc2
root <root@Alpha.localdomain>
2020-11-09 01:56:37 +0200

    Basic form

--- frontend/predict-covid/src/App.js
+++ frontend/predict-covid/src/App.js
@@ -1,38 +1,89 @@
+import React, { Component } from 'react';
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button, Row
+} from 'reactstrap';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from "@material-ui/core/Typography";
-import Form from './Form';
 
-function App() {
+class App extends Component {
 
-  return (
-    <div className="App">
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '100vh'}}
-      >
 
-        <Grid item xs={3}>
-          <Card className="main-card">
-            <CardHeader  title="Covid Checker"></CardHeader>
-            <CardContent>
-              <Form>
+  render() {
+    const symptoms = ['Cough', 'Fever', 'Sleepiness', 'Lose of smell and taste']
 
-              </Form>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </div>
-  );
+    return (
+      <Container className="App">
+        <h2>Covid Checker</h2>
+        <Form className="form">
+          <Row>
+            <Col>
+              <FormGroup>
+                <Label for="age">Age</Label>
+                <Input
+                  type="number"
+                  name="age"
+                  id="age"
+                  placeholder="18"
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup tag="fieldset">
+                <Label>Traveled outside the country</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="travel" value="1" /> Yes </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="travel" value="0" /> No </Label>
+                </FormGroup>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <FormGroup tag="fieldset">
+                <Label>Gender</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="gender" value="1" /> Male </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="gender" value="0" /> Female </Label>
+                </FormGroup>
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup tag="fieldset">
+                <Label>Contact with confirmed person</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="contact" value="1" /> Yes </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="contact" value="0" /> No </Label>
+                </FormGroup>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Label>Symptoms</Label>
+          <FormGroup check inline>
+              {symptoms.map((value, index) => {
+                return <Label check key={index}>
+                    <Input type="checkbox"/>{value}</Label>
+              })}
+            </FormGroup>
+          </Row>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- frontend/predict-covid/src/Form.js
+++ frontend/predict-covid/src/Form.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControl from '@material-ui/core/FormControl'
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-}));
+const Form = () => {
+return (
+<MDBContainer>
+  <MDBRow>
+    <MDBCol md="6">
+      <form>
+        <p className="h4 text-center mb-4">Sign in</p>
+        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+          Your email
+        </label>
+        <input type="email" id="defaultFormLoginEmailEx" className="form-control" />
+        <br />
+        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+          Your password
+        </label>
+        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
+        <div className="text-center mt-4">
+          <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+        </div>
+      </form>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+);
+};
 
-export default function Form() {
-    const classes = useStyles();
-
-    return (
-        <div></div>
-    );
-}
+export default Form;
